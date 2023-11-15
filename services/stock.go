7d1ed1af5f25ecd05f7f3c795aef0fd12016058c
oykos-development-hub <hub@oykos.me>
2023-11-15 14:23:32 +0000

    Update code from GitLab

--- services/stock.go
+++ services/stock.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"fmt"
+
 	"gitlab.sudovi.me/erp/accounting-api/data"
 	"gitlab.sudovi.me/erp/accounting-api/dto"
 	"gitlab.sudovi.me/erp/accounting-api/errors"
@@ -82,8 +84,10 @@ func (h *StockServiceImpl) GetStock(id int) (*dto.StockResponseDTO, error) {
 func (h *StockServiceImpl) GetStockList(input *dto.StockFilterDTO) ([]dto.StockResponseDTO, *uint64, error) {
 	conditionAndExp := &up.AndExpr{}
 
-	if input.Title != nil {
-		conditionAndExp = up.And(conditionAndExp, &up.Cond{"title ILIKE": *input.Title})
+	if input.Title != nil && *input.Title != "" {
+		likeCondition := fmt.Sprintf("%%%s%%", *input.Title)
+
+		conditionAndExp = up.And(conditionAndExp, &up.Cond{"title ILIKE": likeCondition})
 	}
 
 	data, total, err := h.repo.GetAll(input.Page, input.Size, conditionAndExp)
